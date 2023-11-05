19f5d70c6e49f4bf83085bff0214ec2e585f67dc
Tokuxo <tokuxocommercial@gmail.com>
2023-11-05 18:12:40 +0800

    Beta Cursor high delay, bout to be fix next commit

--- src/App.js
+++ src/App.js
@@ -4,14 +4,46 @@ import {motion , useAnimationControls} from "framer-motion";
 
 
 export const App = () => {
-
-  const [cookies, setcookie] = useState()
+  
+  const [cookies, setcookie] = useState(0)
+  const [cursor, setcursor] = useState(0)
+  const [test,settest] = useState(0)
   useEffect(() => {
-
+    if (isNaN(parseInt(localStorage.getItem('ckey')))) {
+      localStorage.setItem('ckey', 0)
+    }
+    if (isNaN(parseInt(localStorage.getItem('cukey')))) {
+      localStorage.setItem('cukey', 0)
+    }
     setcookie(parseInt(localStorage.getItem('ckey')));
-
+    setcursor(parseInt(localStorage.getItem('cukey')));
   }, []);
-
+  
+  function newCookie() {
+    setcookie(cookies + 1);
+    localStorage.setItem('ckey', cookies+1);
+    controls.start({ x: -10,y: -10, scale:0.9 })
+    setTimeout(() => {  controls.start({x: 0,y:0,scale: 1}); }, 50);
+  }
+  
+  function Timer() {
+    const [count, setCount] = useState(0);
+  
+    useEffect(() => {
+      setTimeout(() => {
+        setCount(settest(test + 1));
+        if (cursor > 0) {
+        setcookie(cookies + cursor);
+        localStorage.setItem('ckey', cookies+cursor);
+        controls.start({ x: -10,y: -10, scale:0.9 })
+        setTimeout(() => {  controls.start({x: 0,y:0,scale: 1}); }, 50);
+        }
+      }, 1000);
+    }, [test]); // <- add empty brackets here
+  }
+  
+  Timer()
+  
   const controls = useAnimationControls()
   
   function newCookie() {
@@ -26,7 +58,10 @@ export const App = () => {
       
       if (window.confirm("Second Confirm!") === true) {
         setcookie(0)
-        localStorage.setItem('ckey', cookies)
+        localStorage.setItem('ckey', 0)
+        setcursor(0)
+        localStorage.setItem('cukey', 0)
+        
         alert("Score Reseted")
       } else {
         alert("Your Score Is Kept")
@@ -41,7 +76,7 @@ export const App = () => {
   function ChangeTHeme(){
     const CssVar = ['--text','--shadow','--contrest','--theme','--outline']
     const light = ['black','#e8e8e8','white',"var(--Night)",'cyan']
-    const Night = ['white','#2d2d2e','black',"var(--Light)",'#004042']
+    const Night = ['white','#2d2d2e','black',"var(--Light)",'#009ca1']
     if (Theme == false){
       for (let i = 0; i < 5; i++) {
         r.style.setProperty(CssVar[i], light[i]);
@@ -50,12 +85,21 @@ export const App = () => {
       }
     }
     else {
-      for (let i = 0; i < 4; i++) {
+      for (let i = 0; i < 5; i++) {
         r.style.setProperty(CssVar[i], Night[i]);
         console.log("Changed")
         SetTheme(false)
       }
     }
+  }
+  function AddCursors() {
+    if (cookies >= (cursor + 1) * 100){
+      setcursor(cursor+1)
+      setcookie(cookies-(100*(cursor+1)))
+      localStorage.ckey = cookies - (100*(cursor+1));
+      localStorage.setItem('cukey', cursor);
+    }
+    localStorage.setItem('cukey', cursor);
   }
 
   var r = document.querySelector(':root');
@@ -77,7 +121,19 @@ export const App = () => {
           <motion.button whileHover={{ scale:1.1 }} whileTap={{ scale: 0.9 }} className="button" onClick={newCookie}></motion.button>
         </div>
       </div>
-      <div><motion.button whileTap={{ rotate: 40}} className="themebutton" onClick={ChangeTHeme}></motion.button></div>
+      <div>
+        <motion.button whileTap={{ rotate: 40}} className="themebutton" onClick={ChangeTHeme}></motion.button>
+      </div>
+      <br></br>
+      <hr></hr>
+      <div>
+        <motion.button id="upgrade" onClick={AddCursors}>
+          <b>+1 Temperary Cursor</b>  &nbsp;&nbsp;&nbsp;&nbsp;&nbsp; <sub>cost:{(cursor + 1) * 100}</sub>
+        </motion.button>
+        <br></br>
+        <p1 id="cursor">Cursor Count: {cursor}</p1>
+        <p1 id="test">{test}</p1>
+      </div>
       </>
      )
   }
